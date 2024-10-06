62ec612e57200e7293ed21d9663e0eec8374b676
Lucifer <632077567@qq.com>
2024-10-06 20:04:56 +0800

    再往前推进到用户输入这里

---
+++ input&while.py
@@ -0,0 +1,41 @@
+import pandas
+message=input("what do you want to enter？：")
+print(message)
+"""
+1我都懒得说了，input里是出现的消息
+2而input读入的是字符串，如果我想要读入int
+int(内部必须是以有效整数表示的字符串)
+这样来转换一下
+3求模运算符 %
+4 ps:raw_input()来提示用户输入
+5while 条件
+    缩进，然后执行
+6可以使用标志来控制这个玩意一直循环
+active =true
+然后直接
+while active 
+怎么处理active那是别的代码块的问题
+break退出循环
+continue 跳过之后的循环代码块直接执行下一个代码
+x += 1
+无限循环的时候可以按ctrl+c 
+然后可以 traceback most recent call last 最近调用堆栈
+或者就直接关了
+for 中不建议修改元素？
+while lai xiugai yuansu ?
+如果列表里多个相同元素的东西
+那么用 'cat' in pets 
+然后来pets.remove('cat')
+remove似乎只会remove离一开始最近的那个？？
+while 标志
+然后输入
+每次输入都处理一下
+，对比一下输入是不是 某个输入，决定是否暂停输入
+
+"""
+x =1
+while x <= 5:
+    print(x)
+    x +=1
+
+#可以，这一章，非常简单
--- slicebegin.py
+++ slicebegin.py
@@ -1,5 +1,5 @@
 
-players=['charles','marijuana','michael','florence']
+players = [' charles '  , ' marijuana '   , 'michael'  , 'florence']
 print(players[0:3])
 """
 4.4切片，就是使用列表的一部分
@@ -46,10 +46,102 @@ for player in playerest:
 当你在 Python 中查询一个方法但没有调用它时（没有在方法名后加上 ()），
 Python 会返回一个表示该方法的函数对象，而不是其执行结果。这个对象的表示形式就像你看到的那样：
 不是吧
+且或非
+age >= 21 and age >= 21 这个是且
+age >= 21 or age >= 21 这个是或
+
+检查特定值是否包含在列表中
+ 用in关键字
+ 元素  in   列表 
+ 这个expression就是可以判断真假的
+ if 'mushrooms' in requested_players:
+    print("not found")
+elif  'michael' in requested_players:
+    print('found')
+else:
+    print("fine,ok")
+    这三行代码让我深入理解python里的选择结构
+else 是可以省略的
+通过测试后，会直接跳过余下的测试
+所以if if if也在别的情况下非常好用
+if 列表名
+如果列表为空则返回false 如果列表不为空则返回true
+ 
 """
 for player in playerest:
     if player == 'michael':
         print(player.upper)
     else:
         print(player.title())
+requested_players=players
+print('mushrooms' in requested_players)
+print('michael' in requested_players)
+if 'mushrooms' in requested_players:
+    print("not found")
+elif  'michael' in requested_players:
+    print('found')
+else:
+    print("fine,ok")
+"""
+6字典
+用花括号引起
+alien={'color': red , ' points ':5}
+前面是key  后面是value 就是可以用key作为索引，来索引后面的值//具体实现未可知
+
+可以增添
+alien['x_position']= 9
+这样就可以在末尾添加了
+而如果改变的先前的键值对，那么就是赋值
+同理del alien[key]
+alien.pop(paragmeter)必须要有参数啊 eg:print(alien.pop('color'))
+一遍历键值对
+for key,value in alien.items():
+    print('\nKey:'+key)
+    print('Value',value)
+这是循环遍历方式，非常的好用，key 和value一起遍历
+二遍历键
+而for name in alien.keys():
+     print(name)就是直接遍历key，也可以省略掉.keys()因为实际上可以省略，增加代码可读性
+     sorted(alien.keys())临时排序的情况
+三遍历值
+for value in alien.values()就很合理返回一个值的列表然后直接进行遍历
+
+列表和字典的嵌套
+列表的每一个元素都可以是一个字典
+aliens[]
+然后一直append（这个元素）
+str(len(aliens))经典，因为要+
+
+反过来，字典中存储列表
+也就是说，把values值的类型设置为列表了，比如说
+pizza={
+ 'crust':'thick'
+ 'toppings':['mushroooms','extra cheese'
+ }
+ 一般来说不需要缩进，但是还是建议缩进，一个级别，规定一般是一个级别为四个
+而逗号后面一般要留一个空，但前面不留
+
+最后，字典套字典
+users={
+    'aeinstein':{
+        'first': 'albert'
+        },
+    下一个字典
+}
+    
+
+"""
+alien={'color': 'red', ' points ':5}
+print(alien['color'])
+for key,value in alien.items():
+    print('\nKey:'+key)
+    print('Value',value)
+print(alien.pop('color'))
+#这里删除了color
+for name in alien.keys():
+     print(name)
+pizza={
+    'crust': 'thick',
+    'toppings': ['mushroooms', 'extra cheese']
+ }
 
